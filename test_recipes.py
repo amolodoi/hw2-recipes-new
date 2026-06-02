a689bd2c09e95399f8b06f7cf1f32d9940bae94e
Каташов Андрей <adkatashov@edu.hse.ru>
2026-06-02 21:32:16 +0300

    добавил тесты для Recipe

--- test_recipes.py
+++ test_recipes.py
@@ -1,5 +1,5 @@
 import pytest
-from recipes import Ingredient, Recipe, ShoppingList, DietaryRecipe
+from recipes import Ingredient, Recipe
 
 
 class TestIngredient:
@@ -7,19 +7,25 @@ class TestIngredient:
         ing = Ingredient("Мука", 500.0, "г")
         assert ing.name == "Мука"
         assert ing.quantity == 500.0
-        assert ing.unit == "г"
     
     def test_quantity_positive(self):
         with pytest.raises(ValueError):
             Ingredient("Мука", -100, "г")
     
-    def test_str(self):
-        ing = Ingredient("Мука", 500.0, "г")
-        assert str(ing) == "Мука: 500.0 г"
-    
     def test_eq(self):
         ing1 = Ingredient("Мука", 500.0, "г")
         ing2 = Ingredient("Мука", 300.0, "г")
-        ing3 = Ingredient("Сахар", 100.0, "г")
         assert ing1 == ing2
-        assert ing1 != ing3
+
+
+class TestRecipe:
+    def test_add_ingredient(self):
+        recipe = Recipe("Блины")
+        flour = Ingredient("Мука", 500.0, "г")
+        recipe.add_ingredient(flour)
+        assert len(recipe) == 1
+    
+    def test_scale(self):
+        recipe = Recipe("Блины", [Ingredient("Мука", 500.0, "г")])
+        scaled = recipe.scale(2)
+        assert scaled.ingredients[0].quantity == 1000.0
