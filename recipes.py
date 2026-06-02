548697413b711a4bfbd12b6980b89ccc644fc15c
Каташов Андрей <adkatashov@edu.hse.ru>
2026-06-02 15:16:20 +0300

    добавил класс recipe

--- recipes.py
+++ recipes.py
@@ -14,3 +14,21 @@ class Ingredient:
     
     def __eq__(self, other):
         return self.name == other.name and self.unit == other.unit
+
+
+class Recipe:
+    def __init__(self, title, ingredients=None):
+        self.title = title
+        if ingredients is None:
+            self.ingredients = []
+        else:
+            self.ingredients = ingredients
+    
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        result = f"Рецепт: {self.title}"
+        for ing in self.ingredients:
+            result += f"\n  - {ing}"
+        return result
