f9a3300001a368a576e9f1cd199692128d7edea7
Каташов Андрей <adkatashov@edu.hse.ru>
2026-06-02 15:06:33 +0300

    добавил repr и eq для ingredient

--- recipes.py
+++ recipes.py
@@ -8,3 +8,9 @@ class Ingredient:
     
     def __str__(self):
         return f"{self.name}: {self.quantity} {self.unit}"
+    
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+    
+    def __eq__(self, other):
+        return self.name == other.name and self.unit == other.unit
