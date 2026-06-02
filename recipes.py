1b6d8cea096d6347a3e43446a1744a47d7eb072a
Каташов Андрей <adkatashov@edu.hse.ru>
2026-06-02 14:41:57 +0300

    добавил проверку количества в ingredient

--- recipes.py
+++ recipes.py
@@ -1,6 +1,8 @@
 class Ingredient:
     def __init__(self, name, quantity, unit):
         self.name = name
+        if quantity <= 0:
+            raise ValueError("Количество должно быть положительным")
         self.quantity = quantity
         self.unit = unit
     
